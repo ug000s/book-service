3b6bb42206ee9b9f5d940cebb14b622108b4ca7c
YC <ug000sx4@gmail.com>
2026-06-30 13:33:52 +0200

    Implement removeAuthor service with cascading book deletion and transaction handling

--- src/services/author.service.js
+++ src/services/author.service.js
@@ -1,7 +1,8 @@
 import * as bookRepository from "../repositories/book.repository.js";
+import * as authorRepository from "../repositories/author.repository.js";
 import {sequelize} from "../config/database.js";
 
-export const findBookAuthors = async (isbn)  => {
+export const findBookAuthors = async (isbn) => {
     const book = await bookRepository.findBookById(isbn);
     if (!book) {
         throw new Error(`Book with ISBN ${isbn} not found`);
@@ -16,5 +17,25 @@ export const findBookAuthors = async (isbn)  => {
 }
 
 export const removeAuthor = async (authorName) => {
-    // TODO: Implement removeAuthor service
+    // FIXME implements remove author logic with cascade remove all his books
+    const transaction = await sequelize.transaction();
+    try {
+        const author = await authorRepository.findAuthorById(authorName, {
+            transaction,
+            attributes: {
+                include: ['name', [sequelize.col('birth_date'), 'birthDate']],
+                exclude: ['birth_date']
+            },
+        })
+        if (!author) {
+            throw new Error(`Author with name ${authorName} not found`);
+        }
+        await author.destroy({transaction});
+        await transaction.commit();
+        return author;
+    } catch (e) {
+        await transaction.rollback();
+        console.log('Error removing author', e);
+        throw e;
+    }
 }
