282464e0b1632897c53c9318445b30df20710863
YC <ug000sx4@gmail.com>
2026-06-30 13:46:59 +0200

    Implement findPublishersByAuthor service with author validation and unique publisher retrieval

--- src/services/publisher.service.js
+++ src/services/publisher.service.js
@@ -1,3 +1,10 @@
+import * as authorRepository from "../repositories/author.repository.js";
+
 export const findPublishersByAuthor = async (authorName) => {
-    // TODO: Implement findPublishersByAuthor service
+    const author = await authorRepository.findAuthorById(authorName);
+    if (!author) {
+        throw new Error(`Author with name ${authorName} not found`);
+    }
+    const books = await author.getBooks();
+    return [...new Set(books.map(book => book.publisher))];
 }
