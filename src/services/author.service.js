a4f56d9cc8b1e01a01522e8dff41621a621cef7c
YC <ug000sx4@gmail.com>
2026-06-30 13:07:47 +0200

    Implement findBookAuthors service with detailed author information retrieval

--- src/services/author.service.js
+++ src/services/author.service.js
@@ -1,5 +1,18 @@
+import * as bookRepository from "../repositories/book.repository.js";
+import {sequelize} from "../config/database.js";
+
 export const findBookAuthors = async (isbn)  => {
-    // TODO: Implement findBookAuthors service
+    const book = await bookRepository.findBookById(isbn);
+    if (!book) {
+        throw new Error(`Book with ISBN ${isbn} not found`);
+    }
+    return await book.getAuthors({
+        attributes: {
+            include: ['name', [sequelize.col('birth_date'), 'birthDate']],
+            exclude: ['birth_date']
+        },
+        joinTableAttributes: []
+    });
 }
 
 export const removeAuthor = async (authorName) => {
